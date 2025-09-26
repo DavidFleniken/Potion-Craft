a628f557792dc9d6c176a8a2d11ac150db71db89
metinis <denis.muraska@gmail.com>
2025-09-26 01:29:09 -0400

    fix drop item

--- Assets/Minigames/Potion Craft/Scripts/CauldronScripts/CollisionCauldron_POTIONCRAFT.cs
+++ Assets/Minigames/Potion Craft/Scripts/CauldronScripts/CollisionCauldron_POTIONCRAFT.cs
@@ -1,19 +1,32 @@
+using System;
 using UnityEngine;
 
 public class CollisionCauldron_POTIONCRAFT : MonoBehaviour
 {
+    void HandleItem(GameObject item)
+    {
+        bool itemDropped = item.GetComponent<PickupItem_POTIONCRAFT>().GetItemDropped();
+        bool itemThrown = item.GetComponent<ThrowCurrentItem_POTIONCRAFT>().GetThrown();
+
+        if (itemDropped || itemThrown)
+        {
+            GetComponent<ColorMixing_POTIONCRAFT>().addColor(item.GetComponent<ItemProperties_POTIONCRAFT>().GetColor());
+            Destroy(item.gameObject);
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Item"))
         {
-            bool itemDropped = other.GetComponent<PickupItem_POTIONCRAFT>().GetItemDropped();
-            bool itemThrown = other.GetComponent<ThrowCurrentItem_POTIONCRAFT>().GetThrown();
+            HandleItem(other.gameObject);
+        }
+    }
 
-            if (itemDropped || itemThrown)
-            {
-                GetComponent<ColorMixing_POTIONCRAFT>().addColor(other.GetComponent<ItemProperties_POTIONCRAFT>().GetColor());
-                Destroy(other.gameObject);
-            }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            HandleItem(other.gameObject);
         }
     }
 }
