43e6840c15b973595a19b9f44344ee1aed8b21f5
Eggy <davidfleniken443@gmail.com>
2025-10-09 00:49:56 -0400

    quick fix
    fixed spawn rotation

--- Assets/Minigames/Potion Craft/Scripts/Spawner_POTIONCRAFT.cs
+++ Assets/Minigames/Potion Craft/Scripts/Spawner_POTIONCRAFT.cs
@@ -5,6 +5,8 @@ public class Spawner_POTIONCRAFT : MonoBehaviour
     public PotionType potionType;
     public GameObject prefab;
 
+    Quaternion spawnRot = new Quaternion(0.706984699f, 0, 0, -0.707228839f);
+
     void Start()
     {
         SpawnPotion();
@@ -20,7 +22,7 @@ public class Spawner_POTIONCRAFT : MonoBehaviour
 
     private void SpawnPotion()
     {
-        GameObject instance = Instantiate(prefab, transform.position + Vector3.up * 0.2f, Quaternion.identity);
+        GameObject instance = Instantiate(prefab, transform.position + Vector3.up * 0.2f, spawnRot);
         var props = instance.GetComponent<ItemProperties_POTIONCRAFT>();
         props.initialPotionType = potionType;
     }
